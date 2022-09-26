d29f0074a84fd040df42e78a2ccc5a01d592ecd2
Joakim Hassila <jocke@ordo.one>
2022-09-26 17:40:44 +0200

    Update dependencies.

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
 
     dependencies: [
-        .package(url: "https://github.com/ordo-one/package-benchmark", .upToNextMajor(from: "0.1.0")),
+        .package(url: "https://github.com/ordo-one/package-benchmark", .upToNextMajor(from: "0.2.0")),
        // .package(path: "../package-benchmark")
     ],
 
