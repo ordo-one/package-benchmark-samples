2d506fd15299b00413168b5d2223877226c837e2
Joakim Hassila <jocke@ordo.one>
2022-09-27 11:55:30 +0200

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,63 @@
+// swift-tools-version: 5.6
+
+import PackageDescription
+
+let package = Package(
+    name: "PackageBenchmarkSamples",
+    platforms: [.macOS(.v12)],
+
+    products: [
+        .library(
+            name: "PackageBenchmarkSamples",
+            targets: ["PackageBenchmarkSamples"]),
+    ],
+
+    dependencies: [
+        .package(url: "https://github.com/ordo-one/package-benchmark", .upToNextMajor(from: "0.3.0")),
+    ],
+
+    targets: [
+        .target(
+            name: "PackageBenchmarkSamples",
+            dependencies: []),
+
+        // Sample benchmark executable targets, a few displaying how benchmarks can be split up.
+
+        // Absolute minimal boilerplate
+        .executableTarget(
+            name: "Minimal-Benchmark",
+            dependencies: [
+                .product(name: "BenchmarkSupport", package: "package-benchmark"),
+            ],
+            path: "Benchmarks/Minimal"
+        ),
+
+        // Sample showing wide range of API usage
+        .executableTarget(
+            name: "Samples-Benchmark",
+            dependencies: [
+                .product(name: "BenchmarkSupport", package: "package-benchmark"),
+                "PackageBenchmarkSamples"
+            ],
+            path: "Benchmarks/Samples"
+        ),
+
+        // Some miscellaneous tests for edge conditions
+        .executableTarget(
+            name: "Miscellaneous-Benchmark",
+            dependencies: [
+                .product(name: "BenchmarkSupport", package: "package-benchmark"),
+            ],
+            path: "Benchmarks/Miscellaneous"
+        ),
+
+        // Some test benchmarks on Foundation
+        .executableTarget(
+            name: "Foundation-Benchmark",
+            dependencies: [
+                .product(name: "BenchmarkSupport", package: "package-benchmark"),
+            ],
+            path: "Benchmarks/Foundation"
+        ),
+    ]
+)
---
+++ Sources/PackageBenchmarkSamples/PackageBenchmarkSamples.swift
@@ -0,0 +1,17 @@
+public struct PackageBenchmarkSamples {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
+
+func myInternalDummyCounter(_ count: Int) {
+    var sum: Int = 0
+    for x in 0..<count {
+        sum += x
+    }
+    if Int.random(in: 0...100_000_000) == 4711 { // Make sure optimizer doesn't take us out
+        print("\(sum)")
+    }
+}
+
